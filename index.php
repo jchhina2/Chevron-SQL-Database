734c6256944ce685eb7a64d9ca6211372822908e
Jasraj Chhina <35819986+jchhina2@users.noreply.github.com>
2018-07-05 22:41:04 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name ="viewport" content="width=device-width, initial-scale=1">
+  
+  <title>Chevron Employee/Vendor Database</title>
+  
+</head>
+<body>
+  <h1>Welcome to Chevron!</h1>
+  
+  <ul>
+    <li><a href="create-employee.php"></a> -Add an employee</li>
+    <li><a href="find-employee.php"></a> -Look up an employee</li>
+  </ul>
+  
+  <ul>
+    <li><a href="create-vendor.php"></a> -Add a vendor</li>
+    <li><a href="find-vendor.php"></a> -Look up a vendor</li>
+  </ul>
+</body>
+</html>
